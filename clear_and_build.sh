f05465c8c32e72a8166e8b8b28641b27464c5892
maiquel <maiquel_craash@hotmail.com>
2017-09-28 21:05:23 -0300

    Complementado arquivo de limpeza e construcao automatica para evitar erros.

--- clear_and_build.sh
+++ clear_and_build.sh
@@ -4,6 +4,15 @@ docker-compose rm -s -f
 docker-compose build
 docker-compose up -d
 
+echo ===================================================
+echo Aguarde: configurando o kafka...
+sleep 5s
+echo Aguarde: configurando producer...
+sleep 5s
+echo Aguarde: configurando consumer...
+sleep 5s
+echo PRONTO!
 
+open http://192.168.99.100/
                              
                              
